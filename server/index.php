9f68ad0592422514fb5178108f4279eb64cbf09b
ahmed osman <ahmedmido19782@gmail.com>
2024-08-22 21:17:25 +0300

    FIXS:

--- server/index.php
+++ server/index.php
@@ -1,28 +1,4 @@
 <?php
 
-
-use PDO;
-use PDOException;
-class Database {
-    private $dsn = 'mysql:host=mysql.railway.internal;dbname=railway';
-    private $username = 'root';
-    private $password = 'ihiqJYGzjcMNVmhvmcmoXXAlXlRavOsI';
-    private $conn;
-
-    public function getConnection() {
-        $this->conn = null;
-        try {
-            $this->conn = new PDO($this->dsn, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
-        } catch(PDOException $exception) {
-            echo 'Connection error: ' . $exception->getMessage();
-        }
-
-        return $this->conn;
-    }
-}
-
-
-$connect = new Database();
-$conn = $connect->getConnection();
+echo "Hello, World!";
+?>
