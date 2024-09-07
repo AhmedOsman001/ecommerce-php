56351614ee17f836df09ad25c79b6bbc96629c78
ahmed osman <ahmedmido19782@gmail.com>
2024-09-07 17:45:13 +0300

    Quality modification

--- server/src/config/Database.php
+++ server/src/config/Database.php
@@ -13,10 +13,10 @@ class Database {
 
     public function __construct() {
 
-        $host =  "localhost"; ;
-        $dbname =  "productsdb"; ;
-        $this->username =  "root"; ;
-        $this->password =  "Actor@2000"; ;
+        $host = getenv('MYSQLHOST') ;
+        $dbname = getenv('MYSQLDATABASE') ;
+        $this->username = getenv('MYSQLUSER') ;
+        $this->password = getenv('MYSQLPASSWORD');
         
         $this->dsn = "mysql:host=$host;dbname=$dbname";
     }
